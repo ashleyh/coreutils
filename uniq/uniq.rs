af64d113240875e3d105a5f7e1ecae87acdf2d1f
aph <a.hewson@gmail.com>
2014-01-06 15:05:34 +0000

    formatting

--- uniq/uniq.rs
+++ uniq/uniq.rs
@@ -15,42 +15,42 @@ use std::io::buffered::{BufferedReader, BufferedWriter};
 use std::iter::Peekable;
 
 pub struct LineReader<R> {
-  priv reader: R,
+    priv reader: R,
 }
 
 impl<R: Buffer> Iterator<~[u8]> for LineReader<R> {
-  fn next(&mut self) -> Option<~[u8]> {
-    self.reader.read_line().map( |line| { line.into_bytes() })
-  }
+    fn next(&mut self) -> Option<~[u8]> {
+        self.reader.read_line().map( |line| { line.into_bytes() })
+    }
 }
 
 fn is_dup(a: &~[u8], b: &~[u8]) -> bool {
-  return a.eq(b);
+    return a.eq(b);
 }
 
 /// Advances `it` until `pred` would return `false` on the next element
 fn skip<A, T: Iterator<A>>(it: &mut Peekable<A, T>, pred: |&A| -> bool) {
-  loop {
-    match it.peek() {
-      Some(x) if pred(x) => {},
-      _ => break,
-    };
-    it.next();
-  }
+    loop {
+        match it.peek() {
+            Some(x) if pred(x) => {},
+            _ => break,
+        };
+        it.next();
+    }
 }
 
 fn main() {
-  let mut r = LineReader {
-    reader: BufferedReader::new(stdin()),
-  }.peekable();
-  let mut w = BufferedWriter::new(stdout());
-  loop {
-    let first_line = match r.next() {
-      Some(first_line) => first_line,
-      None => break,
-    };
-    w.write(first_line);
-    skip(&mut r, |next_line| is_dup(&first_line, next_line));
-  }
-  w.flush();
+    let mut r = LineReader {
+        reader: BufferedReader::new(stdin()),
+    }.peekable();
+    let mut w = BufferedWriter::new(stdout());
+    loop {
+        let first_line = match r.next() {
+            Some(first_line) => first_line,
+            None => break,
+        };
+        w.write(first_line);
+        skip(&mut r, |next_line| is_dup(&first_line, next_line));
+    }
+    w.flush();
 }
